78d654726c06c8d0b2f3971f31c319aef320e093
Laura Tonning <github@tonning.dev>
2025-09-30 18:11:31 +0200

    Support Strata 11

--- resources/views/bricks/course.blade.php
+++ resources/views/bricks/course.blade.php
@@ -2,8 +2,8 @@
     use Astrogoat\Courses\Models\Course;
     $brickKey = isset($repeaterBrickName) ? "{$repeaterBrickName}.{$index}.{$brickName}" : $brickName;
 @endphp
-<div>
-    <x-fab::forms.select x-on:change="$wire.set('{{ $brickKey }}', $event.target.value)">
+<div wire:key="{{ $brickKey . 'wrapper' . $this->_contentVariation }}">
+    <x-fab::forms.select x-on:change="$wire.set('{{ $brickKey }}.variations.{{ $this->_contentVariation }}', $event.target.value)">
         <option @if(blank($this->get($brickKey)->getValue())) selected="selected" @endif disabled value="">-- Select a course --</option>
         @foreach(Course::all()->pluck('title', 'id')->toArray() as $id => $title)
             <option @if($this->get($brickKey)->getValue() == $id) selected="selected" @endif value="{{ $id }}">{{ $title }}</option>
